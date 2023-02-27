bd2d800c5b6e6bae9f51b241556fccd22cae76f7
Moginder <mogiegan@gmail.com>
2023-02-27 21:28:43 +0530

    test

---
+++ index.js
@@ -0,0 +1,48 @@
+const express = require("express");
+const app = express();
+require('dotenv').config();
+const cors = require("cors");
+const http = require('http').Server(app);
+const PORT = process.env.PORT || 4000;
+const socketIO = require('socket.io')(http, {
+    cors: {
+        origin: '*'
+    }
+});
+
+app.use(cors({
+  origin: '*'
+}))
+let users = []
+
+socketIO.on('connection', (socket) => {
+    console.log(`⚡: ${socket.id} user just connected!`)  
+    socket.on("message", data => {
+      socketIO.emit("messageResponse", data)
+    })
+
+    socket.on("typing", data => (
+      socket.broadcast.emit("typingResponse", data)
+    ))
+
+    socket.on("newUser", data => {
+      users.push(data)
+      socketIO.emit("newUserResponse", users)
+    })
+ 
+    socket.on('disconnect', () => {
+      console.log('🔥: A user disconnected');
+      users = users.filter(user => user.socketID !== socket.id)
+      socketIO.emit("newUserResponse", users)
+      socket.disconnect()
+    });
+});
+
+app.get("/api", (req, res) => {
+  res.json({message: "Hello"})
+});
+
+   
+http.listen(PORT, () => {
+    console.log(`Server listening on ${PORT}`);
+});
